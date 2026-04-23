e7531a0cbd8926c4105584d2879fdf88ca2102bf
sebastian <slmarcasm@gmail.com>
2026-04-22 19:06:47 -0500

    subiendo problemas

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problemaaaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fechaReferencia = new DateTime(2026, 4, 22);
+            Console.Write("Ingrese su fecha de nacimiento: ");
+            Console.Write("Dia: ");
+            int dia = int.Parse(Console.ReadLine());
+            Console.Write("Mes: ");
+            int mes = int.Parse(Console.ReadLine());
+            Console.Write("Año: ");
+            int anio = int.Parse(Console.ReadLine());
+            DateTime fechaNaciminento = new DateTime(anio, mes, dia);
+            int edad = fechaNaciminento.Year - fechaReferencia.Year;
+            if (fechaReferencia < fechaNaciminento.AddYears(edad))
+            {
+                edad--;
+            }
+            Console.WriteLine($"\nAl {fechaReferencia:dd/MM/yyyy}, su edad es: {edad} años.");
+        }
+    }
+}
---
+++ Program111.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probleme1otro_profesor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese hora (0-23): ");
+            int hora = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese los minutos (0-59): ");
+            int minutos = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese los segundos (0-59): ");
+            int segundo = int.Parse(Console.ReadLine());
+
+            segundo++;
+
+            if(segundo == 60)
+            {
+                segundo = 0;
+                minutos++;
+                if(minutos == 60)
+                {
+                    minutos = 0;
+                    hora++;
+                    if(hora == 24)
+                    {
+                        hora = 0;
+                    }
+                }
+            }
+            Console.WriteLine("\nEl segundo siguiente es:");
+            Console.WriteLine($"{hora:D2}:{minutos:D2}:{segundo:D2}");
+
+
+            {
+
+
+            }
+        } 
+    }
+}
